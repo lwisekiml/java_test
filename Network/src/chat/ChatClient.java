65a5e08c007a9ef40319be25aa84551abb62038d
wisekim <lwisekiml@naver.com>
2020-04-28 16:27:47 +0900

    멀티 채팅

--- Network/src/chat/ChatClient.java
+++ Network/src/chat/ChatClient.java
@@ -1,9 +1,112 @@
 package chat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ChatClient {
-
 	public static void main(String[] args) {
-
+		if(args.length != 2) {
+			System.out.println("사용법 : java ChatClient [이름] [접속할 서버ip]");
+			System.exit(1);
+		}
+		
+		Socket sock = null;
+		BufferedReader br = null;
+		PrintWriter pw = null;
+		boolean endflag = false;
+		
+		try {
+		/*****************************************************************
+		 * 입력받은 ip로 10001번 포트에 접속(args[0] : id, args[1] : 서버 ip)
+		 * 1. 서버에 접속하기 위해 Socket 생성하고, Socket으로부터 InputStream과 OutputStream을 얻어와서
+		 *    각각 Buffered와 PrintWriter 형태로 변환 시킴
+		 *****************************************************************/
+			System.out.println("!!!!!!!!!:"+args[1]);
+			sock = new Socket(args[1], 10003);
+			System.out.println("aaaaaaaa");
+			pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		/*****************************************************************
+		 * 2. 키보드로 부터 입력받기 위한 BufferedReader를 생성한 후,
+		 *    서버로부터 전달된 문자열을 모니터에 출력하는 InputThread 객체를 생성
+		 *****************************************************************/
+			System.out.println("bbbbbbbb");
+			BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+			// 사용자의 id를 서버로 전송
+			pw.println(args[0]);
+			pw.flush();
+			System.out.println("cccccccc");
+			
+			InputThread it = new InputThread(sock, br);
+			it.start();
+		/*****************************************************************
+		 * 3. 키보드로부터 한 줄씩 입력받아 서버에 전송(/quit를 입력받기 전까지)
+		 *****************************************************************/
+			String line = null;
+			
+			while((line = keyboard.readLine()) != null) {
+				pw.println(line);
+				pw.flush();
+				
+				if(line.contentEquals("/quit")) {
+					endflag = true;
+					break;
+				}
+			}
+			System.out.println("클라이언트의 접속을 종료합니다.");
+		}catch (Exception ex) {
+			if(!endflag)
+				System.out.println(ex);
+		}finally {
+			try {
+				if(pw != null)
+					pw.close();
+			}catch (Exception ex) { }
+			
+			try {
+				if(sock != null)
+					sock.close();
+			}catch (Exception ex) { }
+		}
 	}
-
+}
+	
+/*****************************************************************
+ * 4. 서버로부터 전달 받은 문자열을 모니터에 출력하는 InputThread 객체를 생성하여
+ *    BufferedReader와 Socket 객체를 인자로 전달 받음 
+ *****************************************************************/
+class InputThread extends Thread{
+	private Socket sock = null;
+	private BufferedReader br = null;
+	public InputThread(Socket sock, BufferedReader br) {
+		this.sock = sock;
+		this.br = br;
+	}
+	
+	/*****************************************************************
+	 * 5. 서버로부터 문자열을 읽어 들여 모니터에 출력
+	 *****************************************************************/
+	public void run() {
+		try {
+			String line = null;
+			while((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		}catch (Exception e) {
+		}finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+			}catch (Exception ex) { }
+			try {
+				if(sock != null) {
+					sock.close();
+				}
+			}catch (Exception ex) { }
+		}
+	}
 }
--- Network/src/chat/ChatServer.java
+++ Network/src/chat/ChatServer.java
@@ -1,9 +1,117 @@
 package chat;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+
 // http://blog.naver.com/PostView.nhn?blogId=cyberhole&logNo=110133796577&parentCategoryNo=&categoryNo=&viewDate=&isShowPopularPosts=false&from=postView
 public class ChatServer {
 
 	public static void main(String[] args) {
-		
+		try {
+			ServerSocket server = new ServerSocket(10003);
+			System.out.println("접속을 기다립니다.");
+			HashMap hm = new HashMap();
+			while(true) {
+				System.out.println("accept전전전");
+				Socket sock = server.accept();
+				System.out.println("accept 완료");
+				ChatThread chatthread = new ChatThread(sock, hm);
+				chatthread.start();
+			}
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+
+class ChatThread extends Thread{
+	private Socket sock;
+	private String id;
+	private BufferedReader br;
+	private HashMap hm;
+	private boolean initFlag = false;
+
+	public ChatThread(Socket sock, HashMap hm) {
+		this.sock = sock;
+		this.hm = hm;
+		try {
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+			br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			id = br.readLine();
+
+			broadcast(id+"님이 접속했습니다.");
+			System.out.println("접속한 사용자의 아이디는 "+ id + "입니다.");
+
+			synchronized (hm) {
+				hm.put(this.id, pw);
+			}
+			initFlag = true;
+		}catch (Exception ex) {
+			System.out.println(ex);
+		}
 	}
 
+	public void run() {
+		try {
+			String line = null;
+
+			while((line = br.readLine()) != null) {
+				if(line.contentEquals("/quit"))
+					break;
+				if(line.indexOf("/to") == 0) {
+					sendmsg(line);
+				}else {
+					broadcast(id + " : " + line);
+				}
+			}
+		}catch (Exception e) {
+			System.out.println(e);
+		}finally {
+			synchronized (hm) {
+				hm.remove(id);
+			}
+			broadcast(id + " 님이 접속을 종료했습니다.");
+			try {
+				if(sock != null)
+					sock.close();
+			}catch (Exception ex) { }
+		}
+	}
+
+	public void sendmsg(String msg) {
+		int start = msg.indexOf(" ") + 1;
+		int end = msg.indexOf(" ", start);
+		
+		if(end != -1) {
+			String to = msg.substring(start, end); // 상대방 이름
+			String msg2 = msg.substring(end + 1); // 보낼 메세지
+			Object obj = hm.get(to);
+			
+			if(obj != null) {
+				PrintWriter pw = (PrintWriter)obj;
+				pw.println(id + "님이 다음의 귓속말을 보내셨습니다. :" + msg2);
+				pw.flush();
+			}
+		}
+	}
+	
+	public void broadcast(String msg) {
+		synchronized (hm) {
+			Collection collection = hm.values(); // Collection values() : hashmap에 지정된 모든 값을 컬렉션 형태로 반환
+			Iterator iter = collection.iterator();
+			
+			while(iter.hasNext()) { // boolean hasNxt() 읽어 올 요소가 남아있는지 확인
+				PrintWriter pw = (PrintWriter)iter.next(); // 다음 요소를 읽어 옴(전에 hasNext()로 확인하는것이 안전)
+				pw.println(msg);
+				pw.flush();
+			}
+		}
+	}
 }
